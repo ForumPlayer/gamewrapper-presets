ee5d1579969dc82ea4797145d7ab527b9bdbc84c
ForumPlayer <fp+git@forumplayer.dev>
2025-12-13 17:17:46 -0300

    Add preset for (I'm not writting this here, sorry) [Steam:3673060]

---
+++ Steam/3673060.pm
@@ -0,0 +1,33 @@
+package GameWrapper::Preset;    # How to Train Your Cock
+use warnings;
+use strict;
+
+our %FeatureFlags = (
+    'Gamescope' => 1,
+    'MangoHud'  => 0,
+    'MangoApp'  => 0,
+    'Zink'      => 0,
+);
+
+our %GamescopeConfig = (
+    #'game-width'        => 1280,
+    #'game-height'       => 720,
+    #'max-fps'           => 60,
+    #'output-width'      => 0,
+    #'output-height'     => 0,
+    #'fullscreen'        => 0,
+    #'borderless'        => 0,
+    #'expose-wayland'    => 0,
+    #'xwayland-count'    => 1,
+    #'steam-integration' => 0,
+    'keep-size'         => 0
+);
+
+sub PatchEnv {
+    return;    #use Env qw(GAMEWRAPPER);
+}
+
+sub PatchCmdLine {
+    my @cmdLine = (@_);
+    return @cmdLine;
+}
