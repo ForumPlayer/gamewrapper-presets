d5ff01a4ff9a3badfb7773204848bf8ffa3b7752
ForumPlayer <fp+git@forumplayer.dev>
2025-12-13 16:54:19 -0300

    Add preset for Vigil (Steam:3817090)

---
+++ Steam/3817090.pm
@@ -0,0 +1,34 @@
+package GameWrapper::Preset;    # Vigil
+use warnings;
+use strict;
+
+our %FeatureFlags = (
+    'Gamescope' => 0,
+    'MangoHud'  => 0,
+    'MangoApp'  => 0,
+    'Zink'      => 0,
+);
+
+our %GamescopeConfig = (
+    #'game-width'        => 1280,
+    #'game-height'       => 720,
+    #'max-fps'           => 60,
+    #'output-width'      => 0,
+    #'output-height'     => 0,
+    #'fullscreen'        => 0,
+    #'borderless'        => 0,
+    'force-grab-cursor' => 1,
+    #'expose-wayland'    => 0,
+    #'xwayland-count'    => 1,
+    #'steam-integration' => 0,
+    'keep-size'         => 0
+);
+
+sub PatchEnv {
+    return;    #use Env qw(GAMEWRAPPER);
+}
+
+sub PatchCmdLine {
+    my @cmdLine = (@_);
+    return @cmdLine;
+}
